7e4d0687c79394c22eb950f3819a86772a38d193
Morgan Fine <mfine@pivotal.io>
2015-08-11 10:40:51 -0700

    Ability to trigger job via provided flags.
    Signed-off-by: Rob Dimsdale <rdimsdale@pivotal.io>

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world, raspberry fly you fool")
+
+	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	username := flags.String("username", "", "Username for concourse")
+	password := flags.String("password", "", "Password for concourse")
+	url := flags.String("url", "", "base url for concourse")
+	pipeline := flags.String("pipeline", "", "pipeline for concourse")
+	job := flags.String("job", "", "job for concourse")
+	flags.Parse(os.Args[1:])
+
+	fmt.Println("raspberryfly airbourne")
+
+	endpoint := *url + "/pipelines/" + *pipeline + "/jobs/" + *job + "/builds"
+	req, err := http.NewRequest("POST", endpoint, nil)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	req.SetBasicAuth(*username, *password)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	fmt.Printf("resp: %v\n", resp)
 }
